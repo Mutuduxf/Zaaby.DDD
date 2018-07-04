76b8a464d48a3b026d72f131628c0bd81693e438
Mutuduxf <aeondxf@live.com>
2018-07-04 10:39:12 +0800

    Add entity's interface which with key

--- Zaaby.DDD.Abstractions/Domain/IEntity.cs
+++ Zaaby.DDD.Abstractions/Domain/IEntity.cs
@@ -1,11 +1,41 @@
-﻿namespace Zaaby.DDD.Abstractions.Domain
+﻿using System;
+
+namespace Zaaby.DDD.Abstractions.Domain
 {
+    public interface IEntity
+    {
+    }
+
     public interface IEntity<out TId> : IEntity
     {
         TId Id { get; }
     }
 
-    public interface IEntity
+    /// <summary>
+    /// Marker interface for entities with <see cref="T:System.String" /> keys
+    /// </summary>
+    public interface IEntityWithStringKey : IEntity<string>
+    {
+    }
+
+    /// <summary>
+    /// Marker interface for entities with <see cref="T:System.Guid" /> keys
+    /// </summary>
+    public interface IEntityWithGuidKey : IEntity<Guid>
+    {
+    }
+
+    /// <summary>
+    /// Marker interface for entities with <see cref="T:System.Int32" /> keys
+    /// </summary>
+    public interface IEntityWithIntKey : IEntity<int>
+    {
+    }
+
+    /// <summary>
+    /// Marker interface for entities with <see cref="T:System.Int64" /> keys
+    /// </summary>
+    public interface IEntityWithLongKey : IEntity<long>
     {
     }
 }
