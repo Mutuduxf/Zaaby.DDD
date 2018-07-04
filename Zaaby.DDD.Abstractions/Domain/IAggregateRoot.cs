ead02e200696f6b546b340e84bc60b511ece9f16
Mutuduxf <aeondxf@live.com>
2018-07-04 10:38:50 +0800

    Add aggregate root's interface which with key

--- Zaaby.DDD.Abstractions/Domain/IAggregateRoot.cs
+++ Zaaby.DDD.Abstractions/Domain/IAggregateRoot.cs
@@ -1,7 +1,36 @@
-﻿namespace Zaaby.DDD.Abstractions.Domain
+﻿using System;
+
+namespace Zaaby.DDD.Abstractions.Domain
 {
     public interface IAggregateRoot<out TId> : IEntity<TId>
     {
+    }
 
+    /// <summary>
+    /// Marker interface for entities with <see cref="T:System.String" /> keys
+    /// </summary>
+    public interface IAggregateRootWithStringKey : IAggregateRoot<string>
+    {
+    }
+
+    /// <summary>
+    /// Marker interface for entities with <see cref="T:System.Guid" /> keys
+    /// </summary>
+    public interface IAggregateRootWithGuidKey : IAggregateRoot<Guid>
+    {
+    }
+
+    /// <summary>
+    /// Marker interface for entities with <see cref="T:System.Int32" /> keys
+    /// </summary>
+    public interface IAggregateRootWithIntKey : IAggregateRoot<int>
+    {
+    }
+
+    /// <summary>
+    /// Marker interface for entities with <see cref="T:System.Int64" /> keys
+    /// </summary>
+    public interface IAggregateRootWithLongKey : IAggregateRoot<long>
+    {
     }
 }
