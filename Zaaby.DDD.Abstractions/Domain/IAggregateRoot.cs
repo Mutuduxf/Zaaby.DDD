e4ea890480f0677a0984890d164744e580963599
Mutuduxf <aeondxf@live.com>
2018-07-06 11:28:36 +0800

    Add handle

--- Zaaby.DDD.Abstractions/Domain/IAggregateRoot.cs
+++ Zaaby.DDD.Abstractions/Domain/IAggregateRoot.cs
@@ -4,6 +4,7 @@ namespace Zaaby.DDD.Abstractions.Domain
 {
     public interface IAggregateRoot<out TId> : IEntity<TId>
     {
+        void Handle(IDomainEvent domainEvent);
     }
 
     /// <summary>
