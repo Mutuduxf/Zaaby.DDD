626b52401d5b89648d9f4f3ddd164645d1df179f
Mutuduxf <aeondxf@live.com>
2018-08-29 16:53:22 +0800

    Recode

--- Demo/AppleHost/Program.cs
+++ Demo/AppleHost/Program.cs
@@ -16,27 +16,17 @@ namespace AppleHost
         {
             var configBuilder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-//                .AddJsonFile("ApplicationService.json", true, true)
-//                .AddJsonFile("Mongo.json", true, true)
                 .AddJsonFile("RabbitMQ.json", true, true);
-//                .AddJsonFile("Redis.json", true, true);
             var config = configBuilder.Build();
 
-//            var appServiceConfig = config.GetSection("ZaabyApplication").Get<Dictionary<string, List<string>>>();
-//            var mongoConfig = config.GetSection("ZaabeeMongo").Get<MongoDbConfiger>();
             var rabbitMqConfig = config.GetSection("ZaabeeRabbitMQ").Get<MqConfig>();
-//            var redisConfig = config.GetSection("ZaabeeRedis").Get<RedisConfig>();
 
             ZaabyServer.GetInstance()
                 .UseDDD()
-//                .UseZaabyClient(appServiceConfig)
                 .UseZaabyServer<IApplicationService>()
                 .UseEventBus()
-//                .AddSingleton<IZaabeeMongoClient>(p => new ZaabeeMongoClient(mongoConfig))
                 .AddSingleton<IZaabeeRabbitMqClient>(p =>
                     new ZaabeeRabbitMqClient(rabbitMqConfig, new Serializer()))
-//                .AddSingleton<IZaabeeRedisClient, ZaabeeRedisClient>(p =>
-//                    new ZaabeeRedisClient(redisConfig, new Zaabee.Redis.Protobuf.Serializer()))
                 .UseUrls("http://*:5001")
                 .Run();
         }
